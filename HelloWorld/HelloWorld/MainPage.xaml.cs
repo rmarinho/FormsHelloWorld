2d46501648f8f7ebf4f9c8c3f4ada2193dc4a749
Rui Marinho <me@ruimarinho.net>
2020-09-11 14:26:14 +0100

    Enable XAMLC test

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace HelloWorld
 {
@@ -13,6 +7,12 @@ namespace HelloWorld
 		public MainPage()
 		{
 			InitializeComponent();
+#if _XAMLC_
+			lblXamlc.Text = "USING XAMLC";
+#else
+			lblXamlc.Text = "NOT USING XAMLC";
+#endif
+
 		}
 	}
 }
