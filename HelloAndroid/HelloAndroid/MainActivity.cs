3eeefc8b23adbbfce0f2167be81c058f5d7618d1
Rui Marinho <me@ruimarinho.net>
2021-05-12 14:34:51 +0100

    Add Microsoft.Extensions.Hosting to use Builder pattern

--- HelloAndroid/HelloAndroid/MainActivity.cs
+++ HelloAndroid/HelloAndroid/MainActivity.cs
@@ -9,21 +9,78 @@ using Android.Text;
 using Android.Text.Style;
 using Android.Views;
 using Android.Widget;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace HelloAndroid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
+#if __BUILDER__
+			UseBuilder();
+#else
+			UseDefault();
+#endif
+		}
 
-            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
-            var spannable = new SpannableStringBuilder("Learn more at https://aka.ms/xamarin-quickstart");
-            spannable.SetSpan(new StyleSpan(TypefaceStyle.Bold), 14, 47, 0);
-            textView1.SetText(spannable, TextView.BufferType.Spannable);
-        }
+		void UseDefault()
+		{
+			var textService = new TextService();
+			SetText(textService.GetText());
+		}
+
+		void UseBuilder()
+		{
+			var builder = Host.CreateDefaultBuilder();
+			builder.ConfigureServices(collection =>
+			{
+				collection.AddSingleton<ITextService, TextService>();
+				collection.AddSingleton<IHostLifetime, CustomHostLifetime>();
+			});
+			var host = builder.Build();
+			host.Start();
+			var textService = host.Services.GetRequiredService<ITextService>();
+			SetText($"From Builder {textService.GetText()}");
+		}
+
+		void SetText(string text)
+		{
+			TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+			var spannable = new SpannableStringBuilder(text);
+			spannable.SetSpan(new StyleSpan(TypefaceStyle.Bold), 14, 47, 0);
+			textView1.SetText(spannable, TextView.BufferType.Spannable);
+		}
+
+		public interface ITextService
+		{
+			string GetText();
+		}
+
+		public class TextService : ITextService
+		{
+			public string GetText() => "Learn more at https://aka.ms/xamarin-quickstart";
+		}
+
+		public class CustomHostLifetime : IHostLifetime
+		{
+			public string GetText() => "Learn more at https://aka.ms/xamarin-quickstart";
+
+			public Task StopAsync(CancellationToken cancellationToken)
+			{
+				return Task.Run(() => { });
+			}
+
+			public Task WaitForStartAsync(CancellationToken cancellationToken)
+			{
+				return Task.Run(() => { });
+			}
+		}
 	}
 }
