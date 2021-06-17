4b5bad38fcc5e171448c71134ccfbff826bddf14
Rui Marinho <me@ruimarinho.net>
2021-06-17 23:44:15 +0100

    Updates for Preview 5

--- src/HelloMaui/HelloMaui/Startup.cs
+++ src/HelloMaui/HelloMaui/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
 
 namespace HelloMaui
 {
@@ -8,9 +9,9 @@ namespace HelloMaui
 	{
 		public void Configure(IAppHostBuilder appBuilder)
 		{
-			appBuilder
-				.UseFormsCompatibility()
-				.UseMauiApp<App>();
+			//Uncomment for Preview4
+			//appBuilder.UseFormsCompatibility();
+			appBuilder.UseMauiApp<App>();
 				//.ConfigureFonts(fonts =>
 				//{
 				//	fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
